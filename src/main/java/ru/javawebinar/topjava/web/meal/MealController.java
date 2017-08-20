7acc18a0c3acb3b7ba350ffad621abe9df8b0bcb
Stanislav <stasn@ua.fm>
2017-08-20 20:46:14 +0300

    HW task 3 finished with very angry mood

--- src/main/java/ru/javawebinar/topjava/web/meal/MealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealController.java
@@ -5,26 +5,30 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealWithExceed;
+import ru.javawebinar.topjava.util.DateTimeUtil;
 import ru.javawebinar.topjava.util.MealsUtil;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
 
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
 import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
 
-/**
- * Created by Stanislav on 19.08.2017.
- */
+
 @Controller
-@RequestMapping(value = "/meals")
+@RequestMapping(value = "meals")
 public class MealController {
 
     private static final Logger log = LoggerFactory.getLogger(MealRestController.class);
@@ -33,7 +37,7 @@ public class MealController {
     private MealService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public String users(Model model) {
+    public String meals(Model model) {
         int userId = AuthorizedUser.id();
         log.info("getAll for userId={}", userId);
         model.addAttribute("meals", MealsUtil.getWithExceeded(service.getAll(userId), AuthorizedUser.getCaloriesPerDay()));
@@ -50,33 +54,70 @@ public class MealController {
     @RequestMapping(value = "/update", method = RequestMethod.GET)
     public String get(@RequestParam("id") Integer id, Model model) {
         log.info("update meal {} for userId={}", id, AuthorizedUser.id());
-        service.get(id,AuthorizedUser.id());
-        model.addAttribute("meal", service.get(id,AuthorizedUser.id()));
+        service.get(id, AuthorizedUser.id());
+        model.addAttribute("meal", service.get(id, AuthorizedUser.id()));
         return "mealForm";
     }
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public void save(@ModelAttribute("id") Integer id,
-                       @ModelAttribute("dateTime") String dateTime,
-                       @ModelAttribute("description") String descripton,
-                       @ModelAttribute("calories") Integer calories, HttpServletResponse response){
-            Meal meal = new Meal(LocalDateTime.parse(dateTime),descripton,calories);
-            if (id == null) {
-                log.info("create meal for userId={}", AuthorizedUser.id());
-                service.create(meal,AuthorizedUser.id());
-            } else {
-                log.info("update meal {} for userId={}", id, AuthorizedUser.id());
-                assureIdConsistent(meal, id);
-                service.update(meal, AuthorizedUser.id());
-            }
-
-        try {
-            response.sendRedirect("/meals");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public String create(Model model) {
+        log.info("create meal for userId={}", AuthorizedUser.id());
+        model.addAttribute("meal", new Meal());
+        return "mealForm";
     }
 
 
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(@ModelAttribute("id") String id,
+                       @ModelAttribute("dateTime") String dateTime,
+                       @ModelAttribute("description") String descripton,
+                       @ModelAttribute("calories") Integer calories,
+                       HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+        log.info("create meal for Id={}", id);
+        Meal meal = new Meal(LocalDateTime.parse(dateTime), descripton, calories);
+        if (id.equals("")) {
+            log.info("create meal for userId={}", AuthorizedUser.id());
+            service.create(meal, AuthorizedUser.id());
+        } else {
+            log.info("update meal {} for userId={}", id, AuthorizedUser.id());
+            assureIdConsistent(meal, Integer.parseInt(id));
+            service.update(meal, AuthorizedUser.id());
+        }
+//        response.sendRedirect("/meals");
+//        response.setHeader("Location", "/meal");
 
+        return "redirect:/meals";
+    }
+
+    @RequestMapping(value = "filter", method = RequestMethod.POST)
+    public String filter(@ModelAttribute("startDate") String startDate,
+                         @ModelAttribute("endDate") String endDate,
+                         @ModelAttribute("startTime") String startTime,
+                         @ModelAttribute("endTime") String endTime,
+                         Model model,
+                         HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        model.addAttribute("meals", getBetween(parseLocalDate(startDate),
+                parseLocalTime(startTime),
+                parseLocalDate(endDate),
+                parseLocalTime(endTime)));
+
+//        response.setHeader("referer",request.getHeader("referer"));
+//        request.getRequestDispatcher("/WEB-INF/jsp/meals").forward(request, response);
+        return "meals";
+    }
+
+    private List<MealWithExceed> getBetween(LocalDate startDate, LocalTime startTime, LocalDate endDate, LocalTime endTime) {
+        int userId = AuthorizedUser.id();
+        log.info("getBetween dates({} - {}) time({} - {}) for userId={}", startDate, endDate, startTime, endTime, userId);
+
+        return MealsUtil.getFilteredWithExceeded(
+                service.getBetweenDates(
+                        startDate != null ? startDate : DateTimeUtil.MIN_DATE,
+                        endDate != null ? endDate : DateTimeUtil.MAX_DATE, userId),
+                startTime != null ? startTime : LocalTime.MIN,
+                endTime != null ? endTime : LocalTime.MAX,
+                AuthorizedUser.getCaloriesPerDay()
+        );
+    }
 }
--- src/main/webapp/WEB-INF/jsp/meals.jsp
+++ src/main/webapp/WEB-INF/jsp/meals.jsp
@@ -5,14 +5,11 @@
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <html>
 <jsp:include page="fragments/headTag.jsp"/>
-<head>
-    <link rel="stylesheet" href="css/style.css">
-</head>
 <body>
 <jsp:include page="fragments/bodyHeader.jsp"/>
 <section>
     <h3><spring:message code="meal.title"/></h3>
-    <form method="post" action="meals?action=filter">
+    <form method="post" action="${pageContext.request.contextPath}/meals/filter">
         <dl>
             <dt><spring:message code="filter.startDate"/>:</dt>
             <dd><input type="date" name="startDate" value="${param.startDate}"></dd>
@@ -32,7 +29,7 @@
         <button type="submit">Filter</button>
     </form>
     <hr/>
-    <a href="meals?action=create">Add Meal</a>
+    <a href="${pageContext.request.contextPath}/meals/create">Add Meal</a>
     <hr/>
     <table border="1" cellpadding="8" cellspacing="0">
         <thead>
@@ -54,11 +51,12 @@
                 </td>
                 <td>${meal.description}</td>
                 <td>${meal.calories}</td>
-                <td><a href="meals/update/?id=${meal.id}">Update</a></td>
-                <td><a href="meals/delete/?id=${meal.id}">Delete</a></td>
+                <td><a href="${pageContext.request.contextPath}/meals/update/?id=${meal.id}">Update</a></td>
+                <td><a href="${pageContext.request.contextPath}/meals/delete/?id=${meal.id}">Delete</a></td>
             </tr>
         </c:forEach>
     </table>
 </section>
+<jsp:include page="fragments/footer.jsp"/>
 </body>
 </html>
