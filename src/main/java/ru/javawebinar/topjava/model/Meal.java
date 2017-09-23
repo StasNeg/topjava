44073bf4692f0ad9df0aa38921e1ca3c74e19595
Stanislav <stasn@ua.fm>
2017-09-23 22:49:12 +0300

    HW 10.1 - 10.2 AdminAjaxController/MealAjaxController with ExceptionInfoHandler, REST controllers with ExceptionInfoHandler

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -51,7 +51,7 @@ public class Meal extends AbstractBaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
-    @NotNull(groups = View.ValidatedUI.class)
+//    @NotNull(groups = View.ValidatedUI.class)
     private User user;
 
     public Meal() {
